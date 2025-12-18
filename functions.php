1cee3cd79ab3ff197421c13b9745aa1c168bfe58
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2025-12-18 18:02:06 -0500

    setup - enqueued the stylesheet

--- functions.php
+++ functions.php
@@ -8,6 +8,14 @@
  */
 
 /**
+ * Equeuing Stylesheet
+ */
+function kanopi_enqeue() {
+	wp_enqueue_style( 'main-styles', get_stylesheet_uri(), array(), '0.1' );
+}
+add_action( 'wp_enqueue_scripts', 'kanopi_enqeue' );
+
+/**
  * Display Date
  *
  * A shortcode to display a dynamic date string in a template HTML file.
